dc3253ada7e25fa677706d2f5e2574bb386b231d
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-11-28 17:22:10 +0100

    🎉 INITIAL COMMIT

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/hello', (req, res) => {
+  res.json('hello, there');
+});
+
+server.use('*', (req, res) => {
+  res.json({ message: 'API is UP!' })
+});
+
+const port = process.env.PORT || 9000; // fallback in case there is no PORT in either process.env nor the environment
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+});
